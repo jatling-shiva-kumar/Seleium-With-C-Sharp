fb3af024c93eb0fb8be63d38f6ef1f8326040f23
jatling-shiva-kumar <166365395+jatling-shiva-kumar@users.noreply.github.com>
2025-01-13 15:56:12 +0530

    Add files via upload

---
+++ seleniumTest.cs
@@ -0,0 +1,18 @@
+﻿namespace selenium_Test;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+public class seleninTest
+{
+[Test]
+// [Category("Ekalavya")]
+public void FirstTest(){
+    IWebDriver driver = new ChromeDriver();
+    driver.Manage().Window.Maximize();
+    driver.Navigate().GoToUrl("https://ekalavya.online/admin.html");
+    // Assert.That(driver.Title,Is.EqualTo("Ekalavya"));   
+
+}
+
+}
